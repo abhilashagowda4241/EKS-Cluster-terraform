43ed4f518370e83d1d9ea766044581229328747d
abhilashagowda4241 <83212984+abhilashagowda4241@users.noreply.github.com>
2023-05-16 17:13:52 +0530

    variables.tf

---
+++ variables.tf
@@ -0,0 +1,47 @@
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+  type        = string
+}
+
+variable "region" {
+  description = "The aws region. https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/using-regions-availability-zones.html"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "availability_zones_count" {
+  description = "The number of AZs."
+  type        = number
+  default     = 2
+}
+
+variable "project" {
+  description = "Project name"
+  type = string
+}
+
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC."
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_cidr_bits" {
+  description = "The number of subnet bits for the CIDR."
+  type        = number
+  default     = 8
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+  default = {
+    "Project"     = "TerraformEKSWorkshop"
+    "Environment" = "Development"
+  }
+}
